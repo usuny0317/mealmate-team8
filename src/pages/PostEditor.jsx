80edeedd618f801a24e3f0372a07193f441fe514
JoYoungHyun <younghyun550@gmail.com>
2025-02-13 21:53:08 +0900

    Design: 게시글 작성 및 수정 페이지 UI구현 (#5)

--- src/pages/PostEditor.jsx
+++ src/pages/PostEditor.jsx
@@ -1,55 +1,72 @@
 //게시글 작성 및 수정페이지지
 import styled from 'styled-components';
+import { Header } from '../components/common/Header';
 
 const PostEditor = () => {
   return (
-    <StEditorWrapper>
-      <StPostForm>
-        <StLabel>
-          제목
-          <StInput type='text' placeholder='게시글 제목을 입력하세요' />
-        </StLabel>
-        <StLabel>
-          내용
-          <StTextArea placeholder='게시글 내용을 입력하세요' />
-        </StLabel>
-        <StLabel>
-          장소
-          <StInput type='text' placeholder='장소를 입력하세요' />
-        </StLabel>
-        <StLabel>
-          날짜
-          <StInput type='date' />
-        </StLabel>
-        <StLabel>
-          모집인원
-          <StInput type='number' />
-        </StLabel>
-        <StLabel>
-          파일 첨부
-          <StInput type='file' />
-        </StLabel>
-        <StButton type='submit'>게시글 작성</StButton>
-      </StPostForm>
-    </StEditorWrapper>
+    <Root>
+      <Header />
+      <StEditorWrapper>
+        <StPostForm>
+          <StLabel>
+            제목
+            <StInput type='text' placeholder='게시글 제목을 입력하세요' />
+          </StLabel>
+          <StLabel>
+            내용
+            <StTextArea placeholder='게시글 내용을 입력하세요' />
+          </StLabel>
+          <StLabel>
+            장소
+            <StInput type='text' placeholder='장소를 입력하세요' />
+          </StLabel>
+          <StLabel>
+            날짜
+            <StInput type='date' />
+          </StLabel>
+          <StLabel>
+            모집인원
+            <StInput type='number' />
+          </StLabel>
+          <StLabel>
+            파일 첨부
+            <StInput type='file' />
+          </StLabel>
+          <StButton type='submit'>게시글 작성</StButton>
+        </StPostForm>
+      </StEditorWrapper>
+    </Root>
   );
 };
 
 export default PostEditor;
 
+const Root = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
+
 const StEditorWrapper = styled.div`
-  max-width: 70%;
+  height: 80%;
+  width: 100%;
+  max-width: 100%;
   margin: 0 auto;
   padding: 20px;
-  background-color: #f9f9f9;
   border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 50px;
 `;
 
 const StPostForm = styled.form`
+  height: 100%;
+  width: 70%;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  border: 1px solid black;
 `;
 
 const StLabel = styled.label`
