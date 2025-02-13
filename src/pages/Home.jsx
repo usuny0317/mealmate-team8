49b3bf7abdc3c24596a8317279fa6f8e19f9e7b5
Hye10 <110617039+hye410@users.noreply.github.com>
2025-02-13 20:34:14 +0900

    [Feat] alert 공통화 처리

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,37 @@
+import { ALERT_TYPE } from '../constants/alertConstant';
+import { alert } from '../utils/alert';
+
 //메인 페이지
 const Home = () => {
-  return <div>Home</div>;
+  const { SUCCESS, ERROR, WARNING } = ALERT_TYPE;
+  const successAlert = alert();
+  const errorAlert = alert();
+  const warningAlert = alert();
+
+  return (
+    <div>
+      Home
+      <button
+        onClick={() => successAlert({ type: SUCCESS, content: 'ㅊㅋㅊㅋ' })}
+      >
+        성공
+      </button>
+      <button
+        onClick={() =>
+          errorAlert({ type: ERROR, content: 'ㄴㄴ', buttonText: '닫기' })
+        }
+      >
+        실패
+      </button>
+      <button
+        onClick={() =>
+          warningAlert({ type: WARNING, content: '~를 삭제하시겠습니까?' })
+        }
+      >
+        경고
+      </button>
+    </div>
+  );
 };
 
 export default Home;
