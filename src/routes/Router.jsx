80920629acd570b7462a900e5c05a6a8304b22ca
taejin7088 <gog70888@gmail.com>
2025-02-13 20:31:08 +0900

    "feat : #1 헤더 기본디자인과 대략적인이동기능구현"

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -5,20 +5,30 @@ import SignUp from '../pages/SignUp';
 import MyPage from '../pages/MyPage';
 import Detail from '../pages/Detail';
 import PostEditor from '../pages/PostEditor';
+import { Header } from '../components/common/Header';
+import styled from 'styled-components';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<LogIn />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/mypage' element={<MyPage />} />
-        <Route path='/detail' element={<Detail />} />
-        <Route path='/posteditior' element={<PostEditor />} />
-      </Routes>
+      <Header />
+      <StRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<LogIn />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/mypage' element={<MyPage />} />
+          <Route path='/detail' element={<Detail />} />
+          <Route path='/posteditior' element={<PostEditor />} />
+        </Routes>
+      </StRouter>
     </BrowserRouter>
   );
 };
+
+//Header네비바 영역에 안 덮혀지기 작성해놓은 스타일입니다.
+const StRouter = styled.div`
+  padding-top: 60px;
+`;
 
 export default Router;
