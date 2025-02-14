9539dcead3c175178536a16e169722c1bd8f55b8
taejin7088 <gog70888@gmail.com>
2025-02-14 13:24:38 +0900

    feat : #12 마이페이지 안에서 페이지 클릭으로 페이지 이동하는 기능 구현

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,6 +1,69 @@
+import styled from 'styled-components';
+import { Link, Outlet } from 'react-router-dom';
+
 //마이페이지
 const MyPage = () => {
-  return <></>;
+  return (
+    <StMypage>
+      <div>
+        <div className='sidebar'>
+          <ul>
+            <Link to='/mypage/edit-profile'>
+              <li>프로필수정</li>
+            </Link>
+            <Link to='/mypage/my-posts'>
+              <li>내가쓴게시물</li>
+            </Link>
+            <Link to='/mypage/joined-posts'>
+              <li>참여한게시물</li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <div>
+          <Outlet />
+        </div>
+      </div>
+    </StMypage>
+  );
 };
+
+//https://mini-frontend.tistory.com/4
+
+const StMypage = styled.div`
+  display: flex;
+  .sidebar {
+    margin: 10% 0;
+    position: sticky;
+    width: 20vw;
+    min-width: 180px;
+    max-width: 300px;
+    height: 80vh;
+    border-right: 3px solid gray;
+    color: black;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    transition: width 0.3s ease-in-out;
+  }
+  .sidebar ul {
+    list-style: none;
+    padding: 0;
+  }
+  .sidebar li {
+    padding: 10px 0;
+    cursor: pointer;
+  }
+  .sidebar li:hover {
+    background: rgba(255, 255, 255, 0.2);
+  }
+  @media (max-width: 600px) {
+    .sidebar {
+      width: 30vw;
+      min-width: 140px;
+    }
+  }
+`;
 
 export default MyPage;
