02774e76fcfb7028c961711930fdc5233da9b742
JoYoungHyun <younghyun550@gmail.com>
2025-02-14 13:34:32 +0900

    Design: UI 전체 변경(#25)

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,12 +1,25 @@
+import styled from 'styled-components';
+
 export const Header = () => {
   return (
     <>
-      <div
-        style={{ backgroundColor: 'black', color: 'white', height: '50px' }}
-        className=''
-      >
-        header
-      </div>
+      <StHeaderWrapper>header</StHeaderWrapper>
     </>
   );
 };
+
+const StHeaderWrapper = styled.header`
+  width: 100%;
+  padding: 0 15px;
+  box-sizing: border-box;
+  background: black;
+  color: white;
+  position: fixed;
+  top: 0;
+  z-index: 1000;
+
+  @media (max-width: 375px) {
+    // 아이폰 12 Pro 너비(390px) 기준
+    padding: 0 10px;
+  }
+`;
