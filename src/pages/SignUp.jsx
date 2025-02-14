5cffadae4b37888c70d4a0a00d6f13b8a4fb1b6e
usuny0317 <usuny0317@naver.com>
2025-02-14 20:56:50 +0900

    [fetch] 회원가입 페이지에 라디오 버튼과 셀렉트 박스를 만들었습니다. 이후에 성별을 string으로 바꾸고 코드를 다듬을 생각입니다.  #18

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -10,8 +10,13 @@ const Signup = () => {
   const [password, setPassword] = useState('');
   const [nickname, setNickname] = useState('');
   const [main_location, setMain_location] = useState('서울특별시');
-  const [sub_location, setSub_location] = useState([]);
-  const [profile, setProfile] = useState('');
+  const [sub_location, setSub_location] = useState('');
+  const [profile, setProfile] = useState(
+    'https://www.icreta.com/files/attach/images/319/275/055/8e2c1590a474a9afb78c4cb23a9af5b2.jpg'
+  );
+  //select박스에서 두번째 인자 전용입니다!
+  const [get_sub, setget_sub] = useState([]);
+
   const [gender, setGender] = useState(true);
 
   //셀렉트 박스 전용
@@ -23,9 +28,9 @@ const Signup = () => {
   useEffect(() => {
     //메인 비어있을 때 업데이트 null.map 오류 나지 않게..
     if (main_location) {
-      setSub_location(get_sub_select(main_location));
+      setget_sub(get_sub_select(main_location));
     }
-    setSub_location(get_sub_select(main_location));
+    setget_sub(get_sub_select(main_location));
   }, [main_location]);
 
   const handleSignup = async (e) => {
@@ -33,8 +38,6 @@ const Signup = () => {
     //아래는 수파 베이스 값 올려주시면 시도할 것.
     try {
       //수파 베이스 연결 시도하기기
-      console.log(email + ' ' + password + ' ' + nickname + ' ' + gender);
-
       //추가 데이터 보내기
       //서버에서 auth 값(이메일, 비밀번호)을 받아오기
       /* const {data: authData, err: authErr}=await supabase.auth.signUp({
@@ -139,12 +142,16 @@ const Signup = () => {
                 return <option key={main}>{main}</option>;
               })}
             </select>
-            <select className='sub_location'>
+            <select
+              className='sub_location'
+              onChange={(e) => {
+                setSub_location(e.target.value);
+              }}
+            >
               {
                 //배열인지 확인, 배열일 때만 동작하게 && 사용.
-                //문제 발생!! 처음에 서울 눌렀을 때 안된다..
-                Array.isArray(sub_location) &&
-                  sub_location.map((sub) => {
+                Array.isArray(get_sub) &&
+                  get_sub.map((sub) => {
                     return <option key={sub}>{sub}</option>;
                   })
               }
@@ -153,7 +160,20 @@ const Signup = () => {
           <label>
             프로필: <button>추가</button> <button>삭제</button>
           </label>
-          <button type='submit'>가입하기</button>
+          <button
+            type='submit'
+            onClick={() => {
+              console.log('email', email);
+              console.log('password', password);
+              console.log('nick', nickname);
+              console.log('g', gender);
+              console.log('m', main_location);
+              console.log('n', sub_location);
+              console.log('p', profile);
+            }}
+          >
+            가입하기
+          </button>
         </form>
       </div>
     </div>
