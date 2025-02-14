5f3c0a56edd58a03ae3b2217955e1ee40690807e
usuny0317 <usuny0317@naver.com>
2025-02-14 20:10:56 +0900

    [fetch] 회원 가입 페이지에 필요한 변수와 함수를 저장한 파일을 생성했습니다! 또한 회원 가입 페이지에 라디오 버튼과 셀렉트 박스를 만들었습니다. 전체적인 css는 다듬어야 할 것 같습니다~!

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { main_select, get_sub_select } from '../constants/signUpSelector';
 
 //회원가입페이지지
 const Signup = () => {
@@ -8,20 +9,31 @@ const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [nickname, setNickname] = useState('');
-  const [main_location, setMain_location] = useState('');
-  const [sub_location, setSub_location] = useState('');
+  const [main_location, setMain_location] = useState('서울특별시');
+  const [sub_location, setSub_location] = useState([]);
   const [profile, setProfile] = useState('');
   const [gender, setGender] = useState(true);
 
+  //셀렉트 박스 전용
+  const mainselect = main_select;
+
   //완료 후 로그인으로 보내 줄 navigate
   const navigate = useNavigate();
+
+  useEffect(() => {
+    //메인 비어있을 때 업데이트 null.map 오류 나지 않게..
+    if (main_location) {
+      setSub_location(get_sub_select(main_location));
+    }
+    setSub_location(get_sub_select(main_location));
+  }, [main_location]);
 
   const handleSignup = async (e) => {
     e.preventDefault();
     //아래는 수파 베이스 값 올려주시면 시도할 것.
     try {
       //수파 베이스 연결 시도하기기
-      console.log(email + ' ' + password + ' ' + nickname);
+      console.log(email + ' ' + password + ' ' + nickname + ' ' + gender);
 
       //추가 데이터 보내기
       //서버에서 auth 값(이메일, 비밀번호)을 받아오기
@@ -93,8 +105,10 @@ const Signup = () => {
               <input
                 type='radio'
                 value={true}
-                name='남자'
-                onChange={() => {}}
+                name='gender'
+                onChange={(e) => {
+                  setGender(e.target.value);
+                }}
               />
             </label>
             <label>
@@ -102,13 +116,39 @@ const Signup = () => {
               <input
                 type='radio'
                 value={false}
-                name='여성'
-                onChange={() => {}}
+                name='gender'
+                onChange={(e) => {
+                  setGender(e.target.value);
+                }}
               />
             </label>
           </label>
+          <br />
+          {
+            //아래 지역부분을 component로 바꿔서 태진님이랑 공유하기
+          }
           <label>
-            지역: <input onChange={() => {}} />
+            지역:
+            <select
+              className='main_location'
+              onChange={(e) => {
+                setMain_location(e.target.value);
+              }}
+            >
+              {mainselect.map((main) => {
+                return <option key={main}>{main}</option>;
+              })}
+            </select>
+            <select className='sub_location'>
+              {
+                //배열인지 확인, 배열일 때만 동작하게 && 사용.
+                //문제 발생!! 처음에 서울 눌렀을 때 안된다..
+                Array.isArray(sub_location) &&
+                  sub_location.map((sub) => {
+                    return <option key={sub}>{sub}</option>;
+                  })
+              }
+            </select>
           </label>
           <label>
             프로필: <button>추가</button> <button>삭제</button>
