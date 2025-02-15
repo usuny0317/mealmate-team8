d2aa0cf2b9aaf6c44ab09a143192c4e1f5db361e
taejin7088 <gog70888@gmail.com>
2025-02-15 23:05:22 +0900

    feat : 로그인한 유저의 public 유저정보를 뿌려주게함

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -10,7 +10,7 @@ export const AuthProVider = ({ children }) => {
   const errorAlert = alert();
 
   const [isLogin, setIsLogin] = useState(false);
-  const [authUserId, setAuthUserId] = useState('anon');
+  const [loggedInUser, setAuthUserId] = useState('anon');
 
   //일단 로그인을 위해 id비밀번호 문자열로 넣어두겠습니다!
   //닉네임 장현빈(님) 과 연결되어 있는 id비밀번호입니다.
@@ -19,24 +19,38 @@ export const AuthProVider = ({ children }) => {
     useState('1234');
 
   useEffect(() => {
+    let authUserId = '';
     const signUp = async () => {
       const {
         data: { user },
-        error,
+        errorSignUp,
       } = await supabase.auth.signInWithPassword({
         email: supabaseRequestId,
         password: supabaseRequestPassword,
       });
-      if (error) errorAlert({ type: ERROR, content: '로그인실패' });
+      if (errorSignUp) {
+        errorAlert({ type: ERROR, content: '로그인실패 아이디 비밀번호 다름' });
+        return;
+      }
       setIsLogin(!!user);
-      setAuthUserId(user?.id || 'anon');
+      authUserId = user.id;
+      //받아온 auth_Id로 해당유저의 public 유저 정보 가져옴
+      const { data, errorFetchUserData } = await supabase
+        .from('users')
+        .select('*')
+        .eq('id', authUserId)
+        .single();
+      if (errorFetchUserData) {
+        errorAlert({ type: ERROR, content: '서버에러' });
+      }
+      setAuthUserId(data);
     };
     signUp();
   }, []);
   //여기 부분은 로그인 페이지에 있는게 맞는 것 같긴합니다 일단 로그인을 위해 여기에 작성합니다~~
 
   return (
-    <AuthContext.Provider value={{ isLogin, authUserId }}>
+    <AuthContext.Provider value={{ isLogin, loggedInUser }}>
       {children}
     </AuthContext.Provider>
   );
