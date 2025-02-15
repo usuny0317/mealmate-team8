b38b18df302e6ae3eb17050daf7decc3a722f00c
taejin7088 <gog70888@gmail.com>
2025-02-15 21:06:51 +0900

    feat : 로그인 상태와 로그인중인 user의 id를 구분해 두었습니다

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -10,6 +10,7 @@ export const AuthProVider = ({ children }) => {
   const errorAlert = alert();
 
   const [isLogin, setIsLogin] = useState(false);
+  const [authUserId, setAuthUserId] = useState('anon');
 
   //일단 로그인을 위해 id비밀번호 문자열로 넣어두겠습니다!
   //닉네임 장현빈(님) 과 연결되어 있는 id비밀번호입니다.
@@ -19,19 +20,25 @@ export const AuthProVider = ({ children }) => {
 
   useEffect(() => {
     const signUp = async () => {
-      const { data, error } = await supabase.auth.signInWithPassword({
+      const {
+        data: { user },
+        error,
+      } = await supabase.auth.signInWithPassword({
         email: supabaseRequestId,
         password: supabaseRequestPassword,
       });
-      if (error) errorAlert({ type: ERROR, content: error.message });
-      setIsLogin(data);
+      if (error) errorAlert({ type: ERROR, content: '로그인실패' });
+      setIsLogin(!!user);
+      setAuthUserId(user?.id || 'anon');
     };
     signUp();
   }, []);
-  //여기 부분은 로그인 페이지에 있는게 맞는 것 같긴합니다 일단 로그인 로직을 위해 여기에 작성합니다~~
+  //여기 부분은 로그인 페이지에 있는게 맞는 것 같긴합니다 일단 로그인을 위해 여기에 작성합니다~~
 
   return (
-    <AuthContext.Provider value={{ isLogin }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ isLogin, authUserId }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
 
