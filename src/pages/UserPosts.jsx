6e2c42c06ffee0e6882ffc2db6f6387deedced46
taejin7088 <gog70888@gmail.com>
2025-02-16 20:47:01 +0900

    feat : #34 user-posts에 요청된 닉네임이랑 로그인된 닉네임이 같으면 프로필 내 게시물 조회로 이동하는 기능

--- src/pages/UserPosts.jsx
+++ src/pages/UserPosts.jsx
@@ -1,35 +1,42 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PostCard from '../components/home/PostCard';
 import { supabase } from '../supabase/client';
 import { alert } from '../utils/alert';
 import { ALERT_TYPE } from '../constants/alertConstant';
 import { useNavigate, useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import AuthContext from '../context/AuthContext';
 
 const UserPosts = () => {
-  const isLogin = '로그인이되어있습니다.';
+  const { isLogin, loggedInUser } = useContext(AuthContext);
 
   //sweet Alert
   const errorAlert = alert();
   const { ERROR } = ALERT_TYPE;
-
-  const [posts, setPosts] = useState([]);
 
   //패치파라미터로 유저닉네임 가져오기
   const { userNickname } = useParams();
   const navigate = useNavigate();
 
   //로그인이 안되어 있으면 로그인 페이지로 이동
-  if (!isLogin) {
+  if (isLogin) {
     errorAlert({
       type: ERROR,
       content: '로그인이 필요합니다. 로그인 페이지로 이동합니다.',
+    }).then((res) => {
+      if (res.isConfirmed) navigate('/login', { replace: true });
     });
-
-    setTimeout(() => {
-      navigate('/login');
-    }, 0);
+    return <></>;
   }
+
+  //로그인한 유저가 자신의 프로필을 클릭했을시
+  if (loggedInUser.nick_name === userNickname) {
+    navigate('/mypage/my-posts', { replace: true });
+    return <></>;
+  }
+
+  //받아온 포스트를 저장하는 state
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     //받아온 닉네임을 기준으로 포스트리스트에서  가져오기
