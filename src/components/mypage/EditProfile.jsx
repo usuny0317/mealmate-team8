fe9a109716ef2776a32c53b512cc5f458713893d
taejin7088 <gog70888@gmail.com>
2025-02-17 09:30:29 +0900

    feat : 불러오는 지역에 select로 표시되게 수정

--- src/components/mypage/EditProfile.jsx
+++ src/components/mypage/EditProfile.jsx
@@ -3,8 +3,9 @@ import { alert } from '../../utils/alert';
 import { ALERT_TYPE } from '../../constants/alertConstant';
 import AuthContext from '../../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
+import { main_select, sub_select } from '../../constants/signUpSelector';
 export const EditProfile = () => {
-  const { isLogin, loggedInUser } = useContext(AuthContext);
+  const { loggedInUser } = useContext(AuthContext);
   const navigate = useNavigate();
 
   //스위트알럿설정값
@@ -47,14 +48,39 @@ export const EditProfile = () => {
         />
       </div>
       <div>
-        <label htmlFor='nickname'>지역 : </label>
-        <input
-          type='text'
+        지역:
+        <select
+          value={userData.main_location}
+          className='main_location'
+          onChange={(e) => {
+            setUserData({ ...userData, main_location: e.target.value });
+          }}
+        >
+          {main_select.map((main) => {
+            return (
+              <option value={main} key={main}>
+                {main}
+              </option>
+            );
+          })}
+        </select>
+        <select
           value={userData.sub_location}
+          className='sub_location'
           onChange={(e) => {
             setUserData({ ...userData, sub_location: e.target.value });
           }}
-        />
+        >
+          {sub_select[userData.main_location]?.map((sub) => {
+            return (
+              <option value={sub} key={sub}>
+                {sub}
+              </option>
+            );
+          })}
+          {/* {console.log(sub_select[userData.main_location])} */}
+          {/* sub_select,userData.main_location 왜 이건 undefined  */}
+        </select>
       </div>
       <span>성별 : {userData.gender ? '남자' : '여자'}</span>
     </>
