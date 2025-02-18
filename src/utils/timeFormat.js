84b78f6a87ea4e9faed08f0682dd2d78d2432315
Hye10 <110617039+hye410@users.noreply.github.com>
2025-02-18 10:35:33 +0900

    [Chore] 함수 이름 변경 ( getFormatTime -> getFormatDateTime)

--- src/utils/timeFormat.js
+++ src/utils/timeFormat.js
@@ -1,5 +1,5 @@
 // serverDate 형식이 2025-02-22T12:00:00+00:00 이런 형식이어야함!!!
-export const getFormatTime = (serverDate) => {
+export const getFormatDateTime = (serverDate) => {
   const date = serverDate.split('T')[0];
   const time = serverDate.split('T')[1].slice(0, 5);
 
